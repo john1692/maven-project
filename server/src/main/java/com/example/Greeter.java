4cd60df2c999fec642824fe11de5e920a4d5c45b
John Garcia <john.garcia@dxc.com>
2020-01-22 16:32:32 -0500

    fix checkstyle warning 12

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
   }
 
   /**
-  *@param
-  *@return
+  *@param tag for 'someone'
+  *@return tag
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
